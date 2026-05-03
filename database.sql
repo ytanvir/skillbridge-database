0c05dc247a6ef0bb69a1554243820485b5c19bac
Ayesha Akther Haspia <hpaeja840@gmail.com>
2026-05-03 23:20:33 +0600

    Create skills table in Skill.sql
    Created skills table.

--- database.sql
+++ database.sql
@@ -12,3 +12,16 @@ CREATE TABLE IF NOT EXISTS users (
   role ENUM('student','admin') DEFAULT 'student',
   created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   );
+CREATE TABLE IF NOT EXISTS skills (
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    user_id      INT NOT NULL,
+    title        VARCHAR(150) NOT NULL,
+    description  TEXT NOT NULL,
+    category     VARCHAR(80) NOT NULL,
+    tags         VARCHAR(255),
+    skill_type   ENUM('offer','request') DEFAULT 'offer',
+    status       ENUM('active','closed') DEFAULT 'active',
+    views        INT DEFAULT 0,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
